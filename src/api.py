cd606a843d702b1a2bd5fb667c4b21529234735a
rozovata.27 <tanarozova8030@gmail.com>
2026-02-16 18:37:26 +0300

    aft

--- src/api.py
+++ src/api.py
@@ -1,7 +1,8 @@
 from fastapi.routing import APIRouter
 
-from db import add_userr
+from db import add_userr, get_user_by_user_password
 from models import User
+from src.db import re_userr
 
 app = APIRouter()
 
@@ -14,3 +15,15 @@ app = APIRouter()
 @app.post("/add_user")
 async def add_user(user: User):
     return add_userr(user).model_dump()
+
+@app.get("/profile")
+async def profile(user: User):
+    return profile(user).model_dump()
+
+@app.post("/re_profile")
+async def re_profile(user: User):
+    return re_userr(user).model_dump()
+
+@app.post("/get_profile")
+async def get_profile(user: User):
+    return get_user_by_user_password(user).model_dump()
--- src/db.py
+++ src/db.py
@@ -29,9 +29,29 @@ def add_userr(user: User) -> User:
         },
     )
 
-# def get_users (user: User):
-#     cursor = conn.cursor()
-#     cursor.execute(
-#         "INSERT * FROM user"
-#     )
-#     conn.commit()
+
+def re_userr(user: User) -> User:
+    cursor = conn.cursor()
+    cursor.execute(
+        "UPDATE user SET name = %s , PASSWORD = %s WHERE id =  %s ;",
+        (user.name, user.passw, user.id)
+    )
+    conn.commit()
+    return User(
+        **{
+            **user.model_dump(),
+        },
+    )
+
+
+def get_user_by_user_password(user: User) -> User:
+    cursor = conn.cursor()
+    cursor.execute(
+        "SELECT * FROM user WHERE name = %s and password = %s ;",
+        (user.name, user.passw)
+    )
+    user = cursor.fetchall()  # [(2, 'danya', '111')]
+    if len(user) == 1:
+        return User(id=user[0][0], name=user[0][1], passw=user[0][2])
+    else:
+        return User(id = -1, name="-1", passw="-1")
